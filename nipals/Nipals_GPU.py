3f677588a42245b3a9a73a6683b139d220fabb73
AmineDiro <aminedirhoussi1@gmail.com>
2021-02-01 14:21:07 +0100

    GPU functions to run

--- nipals/Nipals_GPU.py
+++ nipals/Nipals_GPU.py
@@ -5,25 +5,15 @@ import pycuda.gpuarray as gpuarray
 from pycuda import cumath
 import numpy as np
 
-
+# GLOBAL VARIABLES
+K = 2
 M = 5
 N = 3
 
-# Allocate memory for the Kth component
-X = np.random.randn(M, N).astype(np.float32)
-th = np.random.randn(M).astype(np.float32)
-ph = np.zeros((N,)).astype(np.float32)
-eigh = np.zeros((1,)).astype(np.float32)
-
-X_gpu = gpuarray.to_gpu(X)
-th_gpu = gpuarray.to_gpu(th)
-ph_gpu = gpuarray.to_gpu(ph)
-eigh_gpu = gpuarray.to_gpu(eigh)
-
-
 # Multiply X.T * th
-mod = SourceModule("""
-    #define N 5
+mult_transpose = SourceModule("""
+    #define N %(size)d
+
     __global__ void mult_transpose(float *X, float *th, float *ph){
         int a_idx = blockIdx.x + threadIdx.x * blockDim.y;
         
@@ -41,18 +31,22 @@ mod = SourceModule("""
         }
 
     }
-    """)
+    """ % {"size": M})
 
-prog = mod.get_function("mult_transpose")
-
-prog(X_gpu, th_gpu, ph_gpu, block=(M, N, 1), grid=(N, 1, 1))
+mult_transpose_gpu = mult_transpose.get_function("mult_transpose")
 
 
-print(ph_gpu)
+def multiply_transpose(X_gpu, th_gpu, ph_gpu):
+    # TODO : define GridSize with some smart shit and reduce the Block size to 32x32
+    input_size = X_gpu.shape
+    mult_transpose_gpu(X_gpu, th_gpu, ph_gpu, block=(M, N, 1), grid=(N, 1, 1))
+    return ph_gpu.get()
 
-mod = SourceModule("""
+
+# Normalize ph/ ||ph||
+normalize = SourceModule("""
     #include <math.h>
-    #define N 3
+    #define N %(size)d
     
     __global__ void normalize(float *ph){
     
@@ -74,18 +68,21 @@ mod = SourceModule("""
      
     }
 
-    """)
+    """ % {"size": N})
 
-prog = mod.get_function("normalize")
-prog(ph_gpu, block=(N, N, 1), grid=(1, 1, 1))
+normalize_gpu = normalize.get_function("normalize")
+
+
+def normalize_ph(ph_gpu):
+    normalize_gpu(ph_gpu, block=(N, N, 1), grid=(1, 1, 1))
+    return ph_gpu.get()
 
 
 # Multiply X * ph
-
 th_old_gpu = th_gpu.copy()
 
-mod = SourceModule("""
-    #define N 3
+mult = SourceModule("""
+    #define N %(size)d
     __global__ void mult(float *X,float *ph, float *th){                
         int idx = threadIdx.x + blockDim.x*blockIdx.x;
         
@@ -103,15 +100,18 @@ mod = SourceModule("""
         }
 
     }
-    """)
+    """ % {"size": N})
+
+mult_gpu = mult.get_function("mult")
 
 
-prog = mod.get_function("mult")
+def multipy(X_gpu, ph_gpu, th_gpu):
+    mult_gpu(X_gpu, ph_gpu, th_gpu, block=(N, 1, 1), grid=(M, 1, 1))
+    return th_gpu.get()
 
-prog(X_gpu, ph_gpu, th_gpu, block=(N, 1, 1), grid=(M, 1, 1))
 
 # Compute eigenvalue
-mod = SourceModule("""
+norm2 = SourceModule("""
      #include <math.h>
      #define M %(size)d
         __global__ void norm2(float *th,float *eigh){                
@@ -134,6 +134,33 @@ mod = SourceModule("""
     }
     """ % {"size": M})
 
-prog = mod.get_function("norm2")
+norm2_gpu = norm2.get_function("norm2")
 
-prog(th_gpu, eigh_gpu, block=(M, 1, 1), grid=(1, 1, 1))
+
+def get_eigenvalue(th_gpu, eigh_gpu):
+    norm2_gpu(th_gpu, eigh_gpu, block=(M, 1, 1), grid=(1, 1, 1))
+    return eigh_gpu.get()
+
+
+if __name__ == '__main__':
+    X = np.random.randn(M, N).astype(np.float32)
+    th = np.random.randn(M).astype(np.float32)
+    ph = np.zeros((N,)).astype(np.float32)
+    eigh = np.zeros((1,)).astype(np.float32)
+
+    X_gpu = gpuarray.to_gpu(X)
+    th_gpu = gpuarray.to_gpu(th)
+    ph_gpu = gpuarray.to_gpu(ph)
+    eigh_gpu = gpuarray.to_gpu(eigh)
+
+    ########## One Comp step ##########
+    # Multiply X.T * th
+    ph = multiply_transpose(X_gpu, th_gpu, ph_gpu)
+    # Normalize ph/ ||ph||
+    ph = normalize_ph(ph_gpu)
+    #Multiply X * ph
+    th = multipy(X_gpu, ph_gpu, th_gpu)
+    # Compute eigenvalue
+    eigh = get_eigenvalue(th_gpu, eigh_gpu)
+
+
